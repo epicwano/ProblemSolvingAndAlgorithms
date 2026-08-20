66264aec9161b9813ea996e90c49f45b20ed0047
Ahmed ALmehdar <ahmed.h.almehdar@gmail.com>
2026-08-20 13:48:22 +0300

    The Complete Veriosn for test the clsQueueLine that do management Queue System

--- RandomProblems/07- Queue Management System/07- Queue Management System.cpp
+++ RandomProblems/07- Queue Management System/07- Queue Management System.cpp
@@ -1,20 +1,38 @@
-// 07- Queue Management System.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include "E:/DS/clsQueueLine.h"
+
+using namespace std;
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+
+	clsQueueLine PayBillsQueue("A0",10);
+
+	PayBillsQueue.IssueTicket();
+	PayBillsQueue.IssueTicket();
+	PayBillsQueue.IssueTicket();
+	PayBillsQueue.IssueTicket();
+	PayBillsQueue.IssueTicket();
+
+	cout << "\n\t\t\t\t\tPay Bills Queue Info : \n";
+	PayBillsQueue.PrintInfo();
+
+	PayBillsQueue.PrintTicketsLineRTL();
+	PayBillsQueue.PrintTicketsLineLTR();
+
+	cout << "\n\t\t\t\t       ---Tickets--- \n";
+	PayBillsQueue.PrintAllTickets();
+
+	PayBillsQueue.ServeNextClient();
+
+	cout << "\n\t\t\t\tPay Bills Queue After Serve one client\n";
+	PayBillsQueue.PrintInfo();
+
+	PayBillsQueue.PrintAllTickets();
+
+	
+
+	system("pause>0");
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
